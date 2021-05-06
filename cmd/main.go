3bdcf82b67a0f9d29d92ae03b286ccf0d004f591
Li Jiayu <jiayu.li@c02wd0fphtdg.sg.corp.seagroup.com>
2021-05-06 15:03:50 +0800

    Handle more session information

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"regexp"
 	"text/template"
@@ -17,7 +18,8 @@ import (
 
 var cookieHandler = securecookie.New(
 	securecookie.GenerateRandomKey(64),
-	securecookie.GenerateRandomKey(32))
+	securecookie.GenerateRandomKey(32),
+)
 
 type User struct {
 	Name     string
@@ -26,23 +28,44 @@ type User struct {
 	Nickname string
 }
 
-func getUser(r *http.Request) (u User) {
+type InfoErr struct {
+	UsernameErr string
+	PasswordErr string
+}
+
+type PageInfo struct {
+	User    User
+	InfoErr InfoErr
+}
+
+func getPageInfo(r *http.Request) (info PageInfo) {
 	var username string
 	var password string
+	var nameErr string
+	var passErr string
 	if cookie, err := r.Cookie("session"); err == nil {
 		cookieValue := make(map[string]string)
 		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
 			username = cookieValue["name"]
 			password = cookieValue["pass"]
+			nameErr = cookieValue["nameErr"]
+			passErr = cookieValue["passErr"]
 		}
 	}
-	return User{Name: username, Password: password}
+	u := User{Name: username, Password: password}
+	ie := InfoErr{UsernameErr: nameErr, PasswordErr: passErr}
+	return PageInfo{
+		User:    u,
+		InfoErr: ie,
+	}
 }
 
-func setSession(u User, w http.ResponseWriter) {
+func setSession(u User, uie InfoErr, w http.ResponseWriter) {
 	value := map[string]string{
-		"name": u.Name,
-		"pass": u.Password,
+		"name":    u.Name,
+		"pass":    u.Password,
+		"nameErr": uie.UsernameErr,
+		"passErr": uie.PasswordErr,
 	}
 	if encoded, err := cookieHandler.Encode("session", value); err == nil {
 		cookie := &http.Cookie{
@@ -67,8 +90,8 @@ func clearSession(w http.ResponseWriter) {
 // templates
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
+func renderUserinfoTemplate(w http.ResponseWriter, tmpl string, info PageInfo) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", info)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -89,21 +112,23 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
 	pass := r.FormValue("password")
 	redirectTarget := "/"
-	if name != "" && pass != "" {
-		// .. check credentials .. TODO
-		if isExistingUsername(name) {
-			fmt.Println("exist!!")
-			if isCorrectPassword(name, pass) {
-				fmt.Println("login successful!")
-			} else {
-				fmt.Println("Fail~")
-			}
+	// .. check credentials .. TODO
+	u := User{Name: name, Password: pass}
+	ie := InfoErr{}
+	if isExistingUsername(name) {
+		log.Printf("User %s found.", name)
+		if isCorrectPassword(name, pass) {
+			log.Printf("Login to %s successful!", name)
+			redirectTarget = "/view"
 		} else {
-			fmt.Println("not exist!!")
+			log.Printf("Login to %s unsuccessful due to wrong password!", name)
+			u.Password = ""
+			ie.PasswordErr = "Incorrect password." // TODO err msg
 		}
-		setSession(User{Name: name, Password: pass}, w)
-		redirectTarget = "/internal"
+	} else {
+		log.Printf("User %s does not exists. Redirect to sign up page.", name)
 	}
+	setSession(u, ie, w)
 	http.Redirect(w, r, redirectTarget, 302)
 }
 
@@ -117,7 +142,8 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 // index page
 
 func indexPageHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index")
+	info := getPageInfo(r)
+	renderUserinfoTemplate(w, "index", info)
 }
 
 // internal page
@@ -132,8 +158,8 @@ const internalPage = `
 `
 
 func internalPageHandler(w http.ResponseWriter, r *http.Request) {
-	u := getUser(r)
-	username := u.Name
+	info := getPageInfo(r)
+	username := info.User.Name
 	if username != "" {
 		fmt.Fprintf(w, internalPage, username)
 	} else {
@@ -148,7 +174,7 @@ var router = mux.NewRouter()
 func main() {
 
 	router.HandleFunc("/", indexPageHandler)
-	router.HandleFunc("/internal", internalPageHandler)
+	router.HandleFunc("/view", internalPageHandler)
 
 	router.HandleFunc("/login", loginHandler).Methods("POST")
 	router.HandleFunc("/logout", logoutHandler).Methods("POST")
