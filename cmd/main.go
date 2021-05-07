035612c5ea5ed47dd36f23f58e7843de237b909d
Li Jiayu <jiayu.li@c02wd0fphtdg.sg.corp.seagroup.com>
2021-05-07 10:30:50 +0800

    Extract router setup methods

--- cmd/main.go
+++ cmd/main.go
@@ -4,41 +4,16 @@ package main
 // https://golang.org/doc/articles/wiki/
 
 import (
-	"fmt"
 	"net/http"
-	"reflect"
 
-	"git.garena.com/jiayu.li/entry-task/cmd/protocol"
 	"github.com/gorilla/mux"
-	"google.golang.org/protobuf/proto"
 )
 
 // server main method
 
 var router = mux.NewRouter()
 
-func main() {
-
-	u := &protocol.User{
-		Name:     "name",
-		Password: "password",
-	}
-	out, err := proto.Marshal(u)
-	if err != nil {
-		fmt.Println("error: ", err)
-	} else {
-		fmt.Println(out)
-		fmt.Println(reflect.TypeOf(out))
-		fmt.Println(string(out))
-		fmt.Println([]uint8(string(out)))
-	}
-	u2 := &protocol.User{}
-	if err := proto.Unmarshal(out, u2); err != nil {
-		fmt.Println("2error: ", err)
-	} else {
-		fmt.Println(u2)
-	}
-
+func setHandleFunc(router *mux.Router) {
 	router.HandleFunc("/", indexPageHandler)
 	router.HandleFunc("/view", viewPageHandler)
 	router.HandleFunc("/signup", signupPageHandler).Methods("GET")
@@ -51,6 +26,11 @@ func main() {
 	router.HandleFunc("/edit", editHandler).Methods("POST")
 	router.HandleFunc("/reset", resetHandler).Methods("POST")
 	router.HandleFunc("/delete", deleteHandler).Methods("POST")
+}
+
+func main() {
+
+	setHandleFunc(router)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":8080", nil)
