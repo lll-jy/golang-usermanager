8a4ac45f36ef6b96de8ded543d6ba66389fc28d2
Li Jiayu <jiayu.li@shopee.com>
2021-05-12 11:57:06 +0800

    Fixed bug of assets image import

--- cmd/paths/paths.go
+++ cmd/paths/paths.go
@@ -8,16 +8,13 @@ var TempPath string
 func SetupPaths(src string) {
 	switch src {
 	case "main":
-		//PlaceholderPath = "backupAssets/placeholder.jpeg"
-		PlaceholderPath = "backupAssets/placeholder.jpeg"
-		//PlaceholderPath = "test/data/original/placeholder.jpeg"
+		PlaceholderPath = "assets/placeholder.jpeg"
 		FileBasePath = "../../../Desktop/EntryTask/entry-task/test/data/upload"
 		FileBaseRelativePath = "test/data/upload"
 		TempPath = "test/data/temp"
 		break
 	case "test":
-		//PlaceholderPath = "../backupAssets/placeholder.jpeg"
-		PlaceholderPath = "../backupAssets/placeholder.jpeg"
+		PlaceholderPath = "../assets/placeholder.jpeg"
 		FileBasePath = "../../../../Desktop/EntryTask/entry-task/test/data/upload"
 		FileBaseRelativePath = "data/upload"
 		TempPath = "data/temp"
