12a59588d59b86ca1d2b3baa688630d3ba7ceead
Li Jiayu <jiayu.li@shopee.com>
2021-05-12 15:15:36 +0800

    Handle coding style issues in initialize helpers

--- cmd/handlers/initialize.go
+++ cmd/handlers/initialize.go
@@ -8,33 +8,39 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Initialize setup the initial database data with 200 users with the following configuration:
+// username = user<i>, password = pass<i*2><i*2>, photo = nil, nickname = nick<i>
 func Initialize(db *sql.DB) {
-	ExecuteQuery(db, "DROP TABLE IF EXISTS users")
-	/*executeQuery(db, `CREATE TABLE users (
-		username    VARCHAR(20) PRIMARY KEY,
-		password    VARCHAR(20) NOT NULL,
-		photo       VARCHAR(50),
-		nickname    VARCHAR(30) COLLATE Latin1_General_100_CI_AI_SC_UTF8
-	)`)*/
-	ExecuteQuery(db, `CREATE TABLE users (
+	err := ExecuteQuery(db, "DROP TABLE IF EXISTS users")
+	if err != nil {
+		log.Printf("Cannot drop table: %s", err.Error())
+	}
+	err = ExecuteQuery(db, `CREATE TABLE users (
 		username    VARCHAR(20) PRIMARY KEY,
 		password    VARCHAR(100) NOT NULL,
 		photo       VARCHAR(50),
 		nickname    VARCHAR(30) COLLATE utf8mb4_unicode_ci
 	)`)
+	if err != nil {
+		log.Printf("Cannot create table: %s", err.Error())
+	}
 	for i := 0; i < 200; i++ {
 		pass, err := bcrypt.GenerateFromPassword([]byte(fmt.Sprintf("pass%d%d", i*2, i*2)), bcrypt.MinCost)
 		if err != nil {
-			log.Printf("Error: password %s cannot be hashed.", pass)
+			log.Printf("Error: password %s cannot be hashed: %s", pass, err.Error())
 		}
-		ExecuteQuery(db, "INSERT INTO users VALUES(?, ?, ?, ?)",
+		err = ExecuteQuery(db, "INSERT INTO users VALUES(?, ?, ?, ?)",
 			fmt.Sprintf("user%d", i),
 			pass, nil,
 			fmt.Sprintf("nick%d", i),
 		)
+		if err != nil {
+			log.Printf("Cannot insert user user%d: %s", i, err.Error())
+		}
 	}
 }
 
+// ExecuteQuery executes the given query (with ? at some places to replace with arguments) to run on db
 func ExecuteQuery(db *sql.DB, query string, args ...interface{}) error {
 	stmt, err := db.Prepare(query)
 	if err != nil {
