3651a592ae7af4fc648a125628570a0e84112faa
Li Jiayu <jiayu.li@shopee.com>
2021-05-12 15:09:58 +0800

    Handle coding style issues in encrypt helpers

--- cmd/handlers/encrypt.go
+++ cmd/handlers/encrypt.go
@@ -10,6 +10,7 @@ import (
 
 // https://www.melvinvivas.com/how-to-encrypt-and-decrypt-data-using-aes/
 
+// prepare prepares the GCM for the password given
 func prepare(pass string) cipher.AEAD {
 	key := make([]byte, 32)
 	length := len(pass)
@@ -22,32 +23,34 @@ func prepare(pass string) cipher.AEAD {
 	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		log.Printf("Cannot crate cipher block from key %v.", key)
+		log.Printf("Cannot crate cipher block from key %v: %s", key, err.Error())
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		log.Printf("Cannot create new GCM from block %v.", block)
+		log.Printf("Cannot create new GCM from block %v: %s", block, err.Error())
 	}
 	return gcm
 }
 
+// encrypt encrypts bytes given with password as key to resulting bytes
 func encrypt(toEncrypt []byte, pass string) []byte {
 	gcm := prepare(pass)
 
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		log.Printf("Cannot create nonce from gcm %v.", nonce)
+		log.Printf("Cannot create nonce from gcm %v: %s", nonce, err.Error())
 	}
 	return gcm.Seal(nonce, nonce, toEncrypt, nil)
 }
 
-func decrypt(encrpyted []byte, pass string) []byte {
+// decrypt decrypts bytes given with password as key to original bytes
+func decrypt(encrypted []byte, pass string) []byte {
 	gcm := prepare(pass)
 	nonceSize := gcm.NonceSize()
-	nonce, ciphered := encrpyted[:nonceSize], encrpyted[nonceSize:]
+	nonce, ciphered := encrypted[:nonceSize], encrypted[nonceSize:]
 	decrypted, err := gcm.Open(nil, nonce, ciphered, nil)
 	if err != nil {
-		log.Printf("Cannot decrypt data.")
+		log.Printf("Cannot decrypt data: %s", err.Error())
 	}
 	return decrypted
 }
