350f9355b49550e747b4952461b619c517bd9655
Li Jiayu <jiayu.li@shopee.com>
2021-05-13 17:51:14 +0800

    Modify logging with levels in action handler

--- cmd/handlers/actionHandlers.go
+++ cmd/handlers/actionHandlers.go
@@ -8,7 +8,6 @@ import (
 	"git.garena.com/jiayu.li/entry-task/cmd/logging"
 	"golang.org/x/crypto/bcrypt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -53,29 +52,29 @@ func LoginHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if protocol.IsExistingUsername(db, name, &u) {
 		logging.Log(logging.INFO, fmt.Sprintf("User %s found.", name))
 		if protocol.IsCorrectPassword(pass, u.Password) {
-			log.Printf("Login to %s successful!", name)
+			logging.Log(logging.INFO, fmt.Sprintf("Login to %s successful!", name))
 			u.Name = name
 			err := DecryptPhoto(u.PhotoUrl, pass, name, &photo)
 			if err != nil {
-				log.Printf(err.Error())
+				logging.Log(logging.ERROR, fmt.Sprintf(err.Error()))
 			}
 			tu.PhotoUrl = u.PhotoUrl
 			tu.Nickname = u.Nickname
 			redirectTarget = "/view"
 			user, err := proto.Marshal(&u)
 			if err != nil {
-				log.Printf("User %v cannot be parsed as a user: %s", &u, err.Error())
+				logging.Log(logging.DEBUG, fmt.Sprintf("User %v cannot be parsed as a user: %s", &u, err.Error()))
 			}
 			header.Set("user", string(user))
 			header.Set("status", "successful login")
 		} else {
-			log.Printf("Login to %s unsuccessful due to wrong password!", name)
+			logging.Log(logging.INFO, fmt.Sprintf("Login to %s unsuccessful due to wrong password!", name))
 			tu.Password = ""
 			ie.PasswordErr = "Incorrect password."
 			header.Set("status", "incorrect password")
 		}
 	} else {
-		log.Printf("User %s does not exists. Redirect to sign up page.", name)
+		logging.Log(logging.INFO, fmt.Sprintf("User %s does not exists. Redirect to sign up page.", name))
 		redirectTarget = "/signup"
 		header.Set("status", "user not exist")
 	}
@@ -86,11 +85,11 @@ func LoginHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 func LogoutHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	info := GetPageInfo(r)
 	clearSession(w)
-	log.Printf("User %s logged out.", info.User.Name)
+	logging.Log(logging.INFO, fmt.Sprintf("User %s logged out.", info.User.Name))
 	if info.Photo != "" && info.Photo != paths.PlaceholderPath {
 		err := os.Remove(info.Photo)
 		if err != nil {
-			log.Printf("Removing file %s unsuccessful: %s", info.Photo, err.Error())
+			logging.Log(logging.DEBUG, fmt.Sprintf("Removing file %s unsuccessful: %s", info.Photo, err.Error()))
 		}
 	}
 	http.Redirect(w, r, "/", 302)
@@ -112,22 +111,23 @@ func userInfoHandler(db *sql.DB, w http.ResponseWriter, r *http.Request, rt stri
 	ie := &InfoErr{}
 	if protocol.IsValidUsername(name) {
 		if protocol.IsExistingUsername(db, name, u) {
-			log.Printf("User signup failure: duplicate user %s found.", name)
+			logging.Log(logging.INFO, fmt.Sprintf("User signup failure: duplicate user %s found.", name))
 			ie.NameErr = fmt.Sprintf("The username %s already exists.", name)
 			header.Set("status", "user already exists")
 		} else if protocol.IsValidPassword(pass) {
 			if pass == repeatPass {
 				if rt == "/signup" {
-					log.Printf("New user %s signed up.", name)
+					logging.Log(logging.INFO, fmt.Sprintf("New user %s signed up.", name))
 				}
 				hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.MinCost)
 				if err != nil {
-					log.Printf("Password %s cannot be hashed: %s", pass, err.Error())
+					logging.Log(logging.ERROR, fmt.Sprintf("Password %s cannot be hashed: %s", pass,
+						err.Error()))
 				}
 				err = ExecuteQuery(db, query, name, hashed, tu.Name)
 				if err != nil {
-					log.Printf("Query %s cannot be executed with arguments %s, %s, %s: %s",
-						query, name, hashed, tu.Name, err.Error())
+					logging.Log(logging.DEBUG, fmt.Sprintf("Query %s cannot be executed with arguments %s, " +
+						"%s, %s: %s", query, name, hashed, tu.Name, err.Error()))
 				}
 				if rt == "/reset" {
 					u.Name = ""
@@ -138,43 +138,43 @@ func userInfoHandler(db *sql.DB, w http.ResponseWriter, r *http.Request, rt stri
 				if info.Photo != "" && info.Photo != paths.PlaceholderPath {
 					err := os.Remove(u.PhotoUrl)
 					if err != nil {
-						log.Printf("The original file path is invalid: %s", err.Error())
+						logging.Log(logging.DEBUG, fmt.Sprintf("The original file path is invalid: %s", err.Error()))
 					}
 					fileBytes, err := ioutil.ReadFile(info.Photo)
 					if err != nil {
-						log.Printf("The temporary file cannot be read properly: %s", err.Error())
+						logging.Log(logging.ERROR, fmt.Sprintf("The temporary file cannot be read properly: %s", err.Error()))
 					}
 					err = ioutil.WriteFile(u.PhotoUrl, encrypt(fileBytes, pass), 0600)
 					if err != nil {
-						log.Printf("The file cannot be re-encrypted: %s", err.Error())
+						logging.Log(logging.ERROR, fmt.Sprintf("The file cannot be re-encrypted: %s", err.Error()))
 					}
-					log.Printf("The original file key is updated.")
+					logging.Log(logging.INFO, fmt.Sprintf("The original file key is updated."))
 				}
 				err = DecryptPhoto(u.PhotoUrl, pass, name, &info.Photo)
 				if err != nil {
-					log.Printf("Cannot decrypt and copy photo %s: %s", u.PhotoUrl, err.Error())
+					logging.Log(logging.ERROR, fmt.Sprintf("Cannot decrypt and copy photo %s: %s", u.PhotoUrl, err.Error()))
 				}
 				tu.Nickname = u.Nickname
 				redirectTarget = tgt
 				user, err := proto.Marshal(u)
 				if err != nil {
-					log.Printf("User %v cannot be parsed as a user: %s", &u, err.Error())
+					logging.Log(logging.DEBUG, fmt.Sprintf("User %v cannot be parsed as a user: %s", &u, err.Error()))
 				}
 				header.Set("user", string(user))
 				header.Set("status", "successful signup")
 			} else {
-				log.Printf("User signup/reset failure: password does not match.")
+				logging.Log(logging.INFO, fmt.Sprintf("User signup/reset failure: password does not match."))
 				ie.PasswordRepeatErr = "The password does not match."
 				header.Set("status", "mismatch password")
 			}
 		} else {
-			log.Printf("User signup/reset failure: password format invalid.")
+			logging.Log(logging.INFO, fmt.Sprintf("User signup/reset failure: password format invalid."))
 			u.Name = name
 			ie.PasswordErr = "The password is not valid."
 			header.Set("status", "wrong password format")
 		}
 	} else {
-		log.Printf("User signup/reset failture: invalid username format of %s.", name)
+		logging.Log(logging.INFO, fmt.Sprintf("User signup/reset failture: invalid username format of %s.", name))
 		ie.NameErr = "The username format is not valid."
 		header.Set("status", "wrong username format")
 	}
@@ -191,7 +191,7 @@ func ResetHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if info.User.Password != "" {
 		userInfoHandler(db, w, r, "/reset", "/view", "UPDATE users SET username = ?, password = ? WHERE username = ?")
 	} else {
-		log.Printf("Access denied. Redirect to homepage.")
+		logging.Log(logging.INFO, fmt.Sprintf("Access denied. Redirect to homepage."))
 		http.Redirect(w, r, "/", 302)
 	}
 }
@@ -216,15 +216,15 @@ func EditHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 			err := ExecuteQuery(db, "UPDATE users SET photo = ?, nickname = ? WHERE username = ?",
 				photo, nickname, info.User.Name)
 			if err != nil {
-				log.Printf("Execution of query updating user profile failed: %s", err.Error())
+				logging.Log(logging.DEBUG, fmt.Sprintf("Execution of query updating user profile failed: %s", err.Error()))
 			}
-			log.Printf("User information of %s updated.", info.User.Name)
+			logging.Log(logging.INFO, fmt.Sprintf("User information of %s updated.", info.User.Name))
 			if info.User.PhotoUrl != "" && info.User.PhotoUrl != paths.PlaceholderPath {
 				err := os.Remove(info.User.PhotoUrl)
 				if err == nil {
-					log.Printf("Removed original photo from database.")
+					logging.Log(logging.INFO, fmt.Sprintf("Removed original photo from database."))
 				} else {
-					log.Printf("Cannot remove the file %s: %s", info.User.PhotoUrl, err.Error())
+					logging.Log(logging.ERROR, fmt.Sprintf("Cannot remove the file %s: %s", info.User.PhotoUrl, err.Error()))
 				}
 			}
 			info.User.PhotoUrl = protocol.ConvertToString(photo)
@@ -233,7 +233,7 @@ func EditHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "/view", 302)
 		}
 	} else {
-		log.Printf("Access denied. Redirect to homepage.")
+		logging.Log(logging.INFO, fmt.Sprintf("Access denied. Redirect to homepage."))
 		http.Redirect(w, r, "/", 302)
 	}
 }
@@ -249,38 +249,38 @@ func UploadHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if info.User.Password != "" {
 		err := r.ParseMultipartForm(10 << 20) // < 10 MB files
 		if err != nil {
-			log.Printf("Cannot parse mutlipart form: %s", err.Error())
+			logging.Log(logging.DEBUG, fmt.Sprintf("Cannot parse mutlipart form: %s", err.Error()))
 		}
 		file, handler, err := r.FormFile("photo_file")
 		if err != nil {
-			log.Printf("Error retrieving file: %s", err.Error())
+			logging.Log(logging.ERROR, fmt.Sprintf("Error retrieving file: %s", err.Error()))
 			header.Set("status", fmt.Sprintf("cannot retrieve: %s ||\n", err))
 		} else {
 			defer file.Close()
-			log.Printf("Photo %s uploaded for user %s. The file size is %+v. MIME header is %+v.",
-				handler.Filename, info.User.Name, handler.Size, handler.Header)
+			logging.Log(logging.INFO, fmt.Sprintf("Photo %s uploaded for user %s. The file size is %+v. " +
+				"MIME header is %+v.", handler.Filename, info.User.Name, handler.Size, handler.Header))
 			targetDir := paths.FileBasePath
 			tempFile, err := ioutil.TempFile(targetDir, "upload-*.jpeg")
 			if err != nil {
-				log.Printf("Error generating temporary file: %s.", err.Error())
+				logging.Log(logging.DEBUG, fmt.Sprintf("Error generating temporary file: %s.", err.Error()))
 			}
 			defer tempFile.Close()
 			fileBytes, err := ioutil.ReadAll(file)
 			if err != nil {
-				log.Printf("Error reading file: %s", err.Error())
+				logging.Log(logging.ERROR, fmt.Sprintf("Error reading file: %s", err.Error()))
 			}
 			_, err = tempFile.Write(encrypt(fileBytes, info.TempUser.Password))
 			if err != nil {
-				log.Printf("Cannot write bytes to file: %s", err.Error())
+				logging.Log(logging.ERROR, fmt.Sprintf("Cannot write bytes to file: %s", err.Error()))
 			}
 			dirs := strings.Split(tempFile.Name(), "/")
 			info.TempUser.PhotoUrl = fmt.Sprintf("%s/%s", paths.FileBaseRelativePath, dirs[len(dirs)-1])
 			err = DecryptPhoto(info.TempUser.PhotoUrl, info.TempUser.Password, info.TempUser.Name, &info.Photo)
 			if err != nil {
-				log.Printf(err.Error())
+				logging.Log(logging.ERROR, fmt.Sprintf(err.Error()))
 			}
 			setSession(info.User, info.TempUser, info.InfoErr, info.Photo, w)
-			log.Println("Successfully uploaded file")
+			logging.Log(logging.INFO, fmt.Sprintln("Successfully uploaded file"))
 			header.Set("tempPhoto", info.Photo)
 			header.Set("status", "success")
 			header.Set("photo", info.TempUser.PhotoUrl)
@@ -297,21 +297,21 @@ func DiscardHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 		if info.TempUser.PhotoUrl != "" && info.TempUser.PhotoUrl != paths.PlaceholderPath {
 			err := os.Remove(info.Photo)
 			if err != nil {
-				log.Printf("Cannot remove file %s: %s", info.Photo, err.Error())
+				logging.Log(logging.DEBUG, fmt.Sprintf("Cannot remove file %s: %s", info.Photo, err.Error()))
 			}
 			err = DecryptPhoto(info.User.PhotoUrl, info.TempUser.Password, info.User.Name, &info.Photo)
 			if err != nil {
-				log.Printf(err.Error())
+				logging.Log(logging.ERROR, fmt.Sprintf(err.Error()))
 			}
-			log.Printf("Temporary file removed.")
+			logging.Log(logging.INFO, fmt.Sprintf("Temporary file removed."))
 		} else {
-			log.Printf("No file to remove.")
+			logging.Log(logging.DEBUG, fmt.Sprintf("No file to remove."))
 		}
 		info.TempUser.PhotoUrl = info.User.PhotoUrl
 		setSession(info.User, info.TempUser, info.InfoErr, info.Photo, w)
 		http.Redirect(w, r, "/view", 302)
 	} else {
-		log.Printf("Access denied. Redirect to homepage.")
+		logging.Log(logging.INFO, fmt.Sprintf("Access denied. Redirect to homepage."))
 		http.Redirect(w, r, "/", 302)
 	}
 }
@@ -321,7 +321,7 @@ func removeFile(src string, validator string) {
 	if validator != "" && validator != paths.PlaceholderPath {
 		err := os.Remove(src)
 		if err != nil {
-			log.Printf("Cannog remove file %s: %s", src, err.Error())
+			logging.Log(logging.DEBUG, fmt.Sprintf("Cannot remove file %s: %s", src, err.Error()))
 		}
 	}
 }
@@ -333,12 +333,12 @@ func RemoveHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 		setSession(info.User, info.TempUser, info.InfoErr, paths.PlaceholderPath, w)
 		err := ExecuteQuery(db, "UPDATE users SET photo = NULL WHERE username = ?", info.User.Name)
 		if err != nil {
-			log.Printf("Cannot remove photo of %s: %s", info.User.Name, err.Error())
+			logging.Log(logging.DEBUG, fmt.Sprintf("Cannot remove photo of %s: %s", info.User.Name, err.Error()))
 		}
 		http.Redirect(w, r, "/edit", 302)
-		log.Printf("Removed profile photo for user %s", info.User.Name)
+		logging.Log(logging.INFO, fmt.Sprintf("Removed profile photo for user %s", info.User.Name))
 	} else {
-		log.Printf("Access denied. Redirect to homepage.")
+		logging.Log(logging.INFO, fmt.Sprintf("Access denied. Redirect to homepage."))
 		http.Redirect(w, r, "/", 302)
 	}
 }
@@ -352,15 +352,14 @@ func DeleteHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 		query := "DELETE FROM users WHERE username = ?"
 		err := ExecuteQuery(db, query, name)
 		if err != nil {
-			log.Printf("Query %s cannot be executed: %s", query, err.Error())
+			logging.Log(logging.ERROR, fmt.Sprintf("Query %s cannot be executed: %s", query, err.Error()))
 			header.Set("status", "cannot delete")
 		} else {
-			log.Printf("User %s deleted.", name)
+			logging.Log(logging.INFO, fmt.Sprintf("User %s deleted.", name))
 			header.Set("status", fmt.Sprintf("delete %s", name))
 		}
 		removeFile(info.Photo, info.Photo)
 		removeFile(info.User.PhotoUrl, info.Photo)
-		log.Printf("User %s deleted.", name)
 		clearSession(w)
 	}
 	http.Redirect(w, r, "/", 302)
